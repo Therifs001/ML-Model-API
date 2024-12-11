a358005300816fd1021b9613893ce63002d90a59
Therifs001 <135795408+Therifs001@users.noreply.github.com>
2024-12-11 21:37:01 +0700

    inferenceService.js

--- src/services/inferenceService.js
+++ src/services/inferenceService.js
@@ -8,50 +8,42 @@ async function predictClassification(model, image) {
             .resizeNearestNeighbor([224, 224])
             .expandDims()
             .toFloat()
-  
+ 
+        const classes = ['Common Rust', 'Gray Leaf Spot', 'Blight','Healthy'];
+ 
         const prediction = model.predict(tensor);
         const score = await prediction.data();
         const confidenceScore = Math.max(...score) * 100;
  
-        const labels = ['Common Rust', 'Gray Leaf Spot', 'Blight', 'Healthy'];
-        const confidenceIndex = scores.indexOf(Math.max(...scores));
-        const label = labels[confidenceIndex];
-
-        let suggestion;
-        switch (label) {
-            case 'Common Rust':
-                suggestion = `
-            Gunakan Fungisida ini:
-            Triazoles: Fungisida dengan bahan aktif seperti propiconazole atau tebuconazole efektif dalam mengatasi infeksi jamur ini.
-            Strobilurins: Azoxystrobin juga dapat membantu mengendalikan infeksi common rust.
-            Contoh produk: Tilt 250EC (Propiconazole), Quadris (Azoxystrobin).
-        `;
-                break;
-            case 'Gray Leaf Spot':
-                suggestion = `
-            Gunakan Fungisida ini:
-            Triazoles seperti propiconazole, tebuconazole, atau difenoconazole sangat efektif untuk pengendalian.
-            Strobilurins seperti azoxystrobin dan pyraclostrobin juga dapat digunakan untuk mengurangi infeksi.
-            Kontak Fungisida: Mancozeb dan Chlorothalonil (misalnya Bravo atau Dithane) dapat digunakan pada kondisi infeksi awal.
-            Contoh produk: Tilt 250EC (Propiconazole), Headline (Pyraclostrobin), Dithane M-45 (Mancozeb).
-        `;
-                break;
-            case 'Blight':
-                suggestion = `
-            Gunakan Fungisida ini:
-            Triazoles (misalnya propiconazole dan tebuconazole) untuk menghambat pertumbuhan jamur.
-            Strobilurins (seperti azoxystrobin atau pyraclostrobin) untuk mengurangi penyebaran infeksi.
-            Contoh produk: Folicur 430SC (Tebuconazole), Quadris (Azoxystrobin).
-        `;
-                break;
-            case 'Healthy':
-                suggestion = 'Tidak ada perawatan yang diperlukan. Tanaman sehat.';
-                break;
+        const classResult = tf.argMax(prediction, 1).dataSync()[0];
+        const label = classes[classResult];
+ 
+        let explanation, suggestion;
+ 
+        if(label === 'Common Rust') {
+            explanation = "Gunakan Fungisida ini:"
+            suggestion = "Triazoles: Fungisida dengan bahan aktif seperti propiconazole atau tebuconazole efektif dalam mengatasi infeksi jamur ini.Strobilurins: Azoxystrobin juga dapat membantu mengendalikan infeksi common rust.Contoh produk: Tilt 250EC (Propiconazole), Quadris (Azoxystrobin)."
         }
  
-        return { label, suggestion};
+        if(label === 'Gray Leaf Spot') {
+            explanation = "Gunakan Fungisida ini:"
+            suggestion = "Triazoles seperti propiconazole, tebuconazole, atau difenoconazole sangat efektif untuk pengendalian.Strobilurins seperti azoxystrobin dan pyraclostrobin juga dapat digunakan untuk mengurangi infeksi.Kontak Fungisida: Mancozeb dan Chlorothalonil (misalnya Bravo atau Dithane) dapat digunakan pada kondisi infeksi awal.Contoh produk: Tilt 250EC (Propiconazole), Headline (Pyraclostrobin), Dithane M-45 (Mancozeb)."
+        }
+ 
+        if(label === 'Blight') {
+            explanation = "Gunakan Fungisida ini:"
+            suggestion = "Triazoles (misalnya propiconazole dan tebuconazole) untuk menghambat pertumbuhan jamur.Strobilurins (seperti azoxystrobin atau pyraclostrobin) untuk mengurangi penyebaran infeksi.Contoh produk: Folicur 430SC (Tebuconazole), Quadris (Azoxystrobin)."
+        
+        }
+        if(label === 'Healthy') {
+            explanation = "Healty."
+            suggestion = "Tidak ada perawatan yang diperlukan. Tanaman sehat."
+        
+        }
+ 
+        return { confidenceScore, label, explanation, suggestion };
     } catch (error) {
-        throw new InputError('Terjadi kesalahan dalam melakukan prediksi')
+        throw new InputError(`Terjadi kesalahan input: ${error.message}`)
     }
 }
  
