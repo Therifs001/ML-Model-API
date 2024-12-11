55097f68ce7a299fd2a70f61353843ecc0ffb03e
Therifs001 <135795408+Therifs001@users.noreply.github.com>
2024-12-11 21:35:32 +0700

    server.js

--- src/server/server.js
+++ src/server/server.js
@@ -1,37 +1,38 @@
 require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const routes = require('../server/routes');
 const loadModel = require('../services/loadModel');
 const InputError = require('../exceptions/InputError');
- 
+
 (async () => {
     const server = Hapi.server({
-        port: process.env.PORT || 8080,
-        host: '0.0.0.0',
+        port: 8080,
+        host: 'localhost',
         routes: {
             cors: {
               origin: ['*'],
             },
         },
     });
- 
+
     const model = await loadModel();
     server.app.model = model;
- 
+
     server.route(routes);
- 
+
     server.ext('onPreResponse', function (request, h) {
         const response = request.response;
- 
+
         if (response instanceof InputError) {
             const newResponse = h.response({
                 status: 'fail',
-                message: `${response.message}`
+                message: `${response.message} Silakan gunakan foto lain.`
             })
             newResponse.code(response.statusCode)
             return newResponse;
         }
- 
+
         if (response.isBoom) {
             const newResponse = h.response({
                 status: 'fail',
@@ -40,10 +41,10 @@ const InputError = require('../exceptions/InputError');
             newResponse.code(response.output.statusCode)
             return newResponse;
         }
- 
+
         return h.continue;
     });
- 
+
     await server.start();
     console.log(`Server start at: ${server.info.uri}`);
 })();
